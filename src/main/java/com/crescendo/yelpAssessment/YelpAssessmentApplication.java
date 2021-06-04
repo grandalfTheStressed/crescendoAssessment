358e622ecd50852e2e85995b2681848caba7e724
Grant Fields <grantfields0725@gmail.com>
2021-06-04 16:18:05 -0500

    init Github

---
+++ src/main/java/com/crescendo/yelpAssessment/YelpAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.crescendo.yelpAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpAssessmentApplication.class, args);
+	}
+
+}
